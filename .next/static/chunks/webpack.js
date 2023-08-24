46be5d207cc298d30428873401dd75e8603db642
Shashi Kumar <shashikumar@Shashis-MacBook-Air.local>
2023-08-24 22:01:05 +0530

    added _app to add global css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1473438f8f307282"; }
+/******/ 		__webpack_require__.h = function() { return "2640c8783efce2b1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2e02fa2685a207df"; }
+/******/ 	__webpack_require__.h = function() { return "41406bb6783f4b0b"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
