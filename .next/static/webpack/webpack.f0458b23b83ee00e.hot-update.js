b46786855e2f0ee1131b789ef724138390c74aff
Shashi Kumar <shashikumar@Shashis-MacBook-Air.local>
2023-08-25 20:25:50 +0530

    added login and signup page

--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "41406bb6783f4b0b"; }
+/******/ 	__webpack_require__.h = function() { return "b9d001c9ce77f2d8"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
